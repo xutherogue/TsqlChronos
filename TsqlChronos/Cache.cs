fb5b7c261b409f3b667eac200802740066903f1f
xutherogue <xutherogue@gmail.com>
2014-10-05 22:26:13 -0400

    Missing files

---
+++ TsqlChronos/Cache.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace TsqlChronos
+{
+    internal sealed class Cache
+    {
+        private readonly IResultProvider provider;
+        private readonly ConcurrentDictionary<int, CacheEntry> entries = new ConcurrentDictionary<int, CacheEntry>();
+
+        public Cache(IResultProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Notify(UpdateStatement statement)
+        {
+            IReadOnlyCollection<CacheEntry> allEntries;
+            lock (this)
+            {
+                allEntries = entries.Select(f => f.Value).ToArray();
+                foreach (var entry in allEntries)
+                    entry.Indict();
+            }
+
+            foreach (var entry in allEntries)
+            {
+                if (Analyzer.IsInvalidated(entry, statement))
+                    entry.Convicted();
+                else
+                    entry.Acquited();
+            }
+        }
+    }
+}
---
+++ TsqlChronos/CacheEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using TsqlChronos.Util;
+
+namespace TsqlChronos
+{
+    public sealed class ResultSet
+    {
+        public object[][] Values;
+    }
+
+    internal interface IResultProvider
+    {
+        Task<ResultSet> GetResults(SelectStatement statement);
+    }
+
+    public sealed class CacheEngine : IResultProvider
+    {
+        private readonly string connectionString;
+        private readonly Cache cache;
+
+        public CacheEngine(string connectionString)
+        {
+            cache = new Cache(this);
+            this.connectionString = connectionString;
+        }
+
+        public void Notify()
+        {
+        }
+
+        public ResultSet Fetch(string query)
+        {
+            return null;
+        }
+
+        public Task<ResultSet> GetResults(SelectStatement statement)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = SqlParser.GetSql(statement);
+                return command.ExecuteReaderAsync().ContinueWith(t => AsResultSet(t.Result));
+            }
+        }
+
+        private static ResultSet AsResultSet(SqlDataReader reader)
+        {
+            var results = new List<object[]>();
+
+            while (reader.Read())
+            {
+                var row = new object[reader.FieldCount];
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                reader.GetValues(row);
+                results.Add(row);
+            }
+
+            return new ResultSet { Values = results.ToArray() };
+        }
+    }
+}
